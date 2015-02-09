1e2f5dfb9a951db6155db7c4ba54c43d279f073d
Quasel <bernhard@gronau.at>
2015-02-09 18:29:21 +0100

    first improvments

--- unitydog.js
+++ unitydog.js
@@ -6,12 +6,15 @@ var unityDog =
 	Setup : function( element )
 	{
 		var webURL		= element.attr( "src" );
-		var	defWidth	= element.attr( "width" );
-		var defHeight	= element.attr( "height" );
-	
-		var unityelement = jQuery( "<div class=\"unitydog standard\"></div>" );
-		
-		// Add the controls
+        var fullscreen = element.attr("fullscreen");
+        // var preview = element.attr("preview");
+
+		var unityelement = jQuery( '<div class="unitydog standard"><div class="missing" style="display:none;"><a href="http://unity3d.com/webplayer/" title="Unity Web Player. Install now!"><img alt="Unity Web Player. Install now!" src="http://webplayer.unity3d.com/installation/getunity.png" width="193" height="63" /></a></div></div>' );
+		unityelement.css("width", element.attr("width") );
+		unityelement.css("height", element.attr("height") );
+
+		// Add the controls @todo improve fullscreen behaviour
+        if(fullscreen != "no"){
 		var controls = jQuery( '<div class="controls"></div>' );
 		
 			var ctrl = jQuery( '<a class="make_fullscreen" ><img src="'+unitydogsettings.fullscreen+'"></a>' );
@@ -21,7 +24,8 @@ var unityDog =
 			var ctrl = jQuery( '<a class="make_normal" ><img src="'+unitydogsettings.restore+'"></a>' );
 				ctrl.click( function(){ unityelement.removeClass( 'fullscreen' ); unityelement.addClass( 'standard' ); jQuery( 'BODY' ).removeClass( 'unitydog_fullscreen' ); } );
 			controls.append( ctrl );
-			
+        }
+		
 		var playbutton = jQuery( '<div class="playbutton">&nbsp;</div>' );
 		playbutton.click( function(){ unityDog.Play( unityelement, webURL ); } );
 		
@@ -41,25 +45,31 @@ var unityDog =
 		// hide our play button
 		unityelement.find( '.playbutton' ).hide();
 
-		player.observeProgress( function ( progress ) 
-		{
-			switch( progress.pluginStatus ) 
-			{
-				case "broken":
-					console.log( "Unity Player broken!" );
-					return;
-					break;
-				case "missing":
-					console.log( "Unity Player missing!" );
-					return;
-					break;
-				case "installed":
-					break;
-				case "first":
-					break;
-			}
-
-		});
+		player.observeProgress(function (progress) {
+            var missingScreen = unityelement.find(".missing");
+            switch(progress.pluginStatus) {
+                case "unsupported":
+                	alert("unity3d Webplayer is not supported on your Platform");
+                break;
+                case "broken":
+                    alert("You will need to restart your browser after installation.");
+                break;
+                case "missing":
+                    missingScreen.find("a").click(function (e) {
+                        e.stopPropagation();
+                        e.preventDefault();
+                        player.installPlugin();
+                        return false;
+                    });
+                    missingScreen.show();
+                break;
+                case "installed":
+                    missingScreen.remove();
+                break;
+                case "first":
+                break;
+            }
+        });
 		
 		var plyelement = jQuery( '<div class="player"></div>' );
 		unityelement.prepend( plyelement );
--- unitydog.php
+++ unitydog.php
@@ -1,9 +1,9 @@
 <?php
 /*
-Plugin Name: 	UnityDog
+Plugin Name: 	UnityDogv2 
 Description: 	Unitydog enabled embedding of unity3d webplayer files in your blog.
-Version: 		1.0.4
-Author: 		Garry Newman
+Version: 		2.0-a-1
+Author: 		Garry Newman, Bernhard Gronau
 Author URI: 	http://www.garry.tv
 License: 		MIT 
 */
@@ -13,7 +13,7 @@ License: 		MIT
 //
 function unitydog_init() 
 {
-	wp_register_script( 'unityobject2', plugins_url( 'unityobject2.js', __FILE__ ) );
+	wp_register_script( 'unityobject2', "http://webplayer.unity3d.com/download_webplayer-3.x/3.0/uo/UnityObject2.js" );
 	wp_register_script( 'unitydog', plugins_url( 'unitydog.js', __FILE__ ) );
 	wp_register_style( 'unitydog', plugins_url( 'unitydog.css', __FILE__ ) );
 }    
@@ -28,6 +28,7 @@ function unitydog_enqueue_style()
    wp_enqueue_script( 'unityobject2' );
    wp_enqueue_script( 'jquery' );
    wp_enqueue_script( 'unitydog' );
+
    wp_enqueue_style( 'unitydog' );
    
 	$params = array
@@ -48,7 +49,7 @@ function unitydog_upload_mimes( $existing_mimes=array() )
 	$existing_mimes['unity3d'] = 'application/vnd.unity'; 
 	return $existing_mimes; 
 }
-add_action( 'upload_mimes', 'unitydog_upload_mimes' );
+add_filter( 'upload_mimes', 'unitydog_upload_mimes' );
 
 //
 // Add a media section for unity3d files
@@ -68,7 +69,7 @@ function unitydog_media_send_to_editor( $html, $id, $attachment )
 	$attachment = get_post( $id );
 	if ( $attachment->post_mime_type != 'application/vnd.unity' ) return $html;
 	
-	return "[unity src=\"".$id."\"]"; 
+	return '[unity src="'.$id.'" width="750" height="563" fullscreen="no"]';
 }
 add_action( 'media_send_to_editor', 'unitydog_media_send_to_editor', 50, 3 );
 
@@ -76,20 +77,25 @@ add_action( 'media_send_to_editor', 'unitydog_media_send_to_editor', 50, 3 );
 //
 // The unity shortcode
 //
-
-function unitydog_unity( $atts ) 
+// @todo add default values
+function unitydog_unity( $atts = array() ) 
 {
 	global $g_UnityPlayerCount;
 	
+	//improve add $defaults 
 	if ( !$atts['src'] ) return "(missing unity src)";
-	
-	$width 	= "\"".$atts['w']."\"";
-	$height	= "\"".$atts['h']."\"";
-	
+
+	$width = $atts['width'];
+	$height = $atts['height'];
 	$url = wp_get_attachment_url( $atts['src'] );
+	// $classes = $atts['class'];
+	$fullscreen = $atts['fullscreen'];
+	// $preview = $atts['preview'];
+
 	if ( !$url ) return "(missing unity attachment)";
+
+	return '<unitydog width="'.$width.'" height="'.$height.'" src="'.$url.'" fullscreen="'.$fullscreen.'">"Pleas enable JavaScript to use Unity3d Webplayer"</unitydog>';
 	
-	return '<unitydog width="'.$width.'" height="'.$height.'" src="'.$url.'"></unitydog>';
 }
 add_shortcode( 'unity', 'unitydog_unity' );
 
@@ -97,6 +103,7 @@ add_shortcode( 'unity', 'unitydog_unity' );
 // A lot of webservers can't serve unity3d files by default.
 // So rename it from .unity to .zip
 //
+// @todo make this optional - deactivated
 function unitydog_add_attachment( $pid )
 {
     $post = get_post( $pid );
@@ -111,4 +118,4 @@ function unitydog_add_attachment( $pid )
     rename( $file, $newfile );    
     update_attached_file( $pid, $newfile );
 }
-add_action( 'add_attachment', 'unitydog_add_attachment' );
+// add_action( 'add_attachment', 'unitydog_add_attachment' );
